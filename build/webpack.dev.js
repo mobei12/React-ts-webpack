2d63ae01db3b204bdeb6144d7edaa1103468f1da
mobei12 <505453856@qq.com>
2024-03-18 18:31:58 +0800

    完善build配置

--- build/webpack.dev.js
+++ build/webpack.dev.js
@@ -1,17 +1,39 @@
+const webpack = require('webpack');
+const WebpackDevServer = require('webpack-dev-server');
+const config = require('./webpack.base');
 const { merge } = require('webpack-merge');
-const baseConfig = require('./webpack.base.js');
-const defConfig = merge(baseConfig, {
+const dotenv = require('dotenv');
+const devEnv = dotenv.config({ path: 'dev.env', override: true });
+const port = process.env.CUSTOMIZE_PORT || 8081; // 你的 devServer 端口号
+const defConfig = merge(config, {
 	mode: 'development',
-	devtool: 'source-map',//开启sourceMap，方便调试
+	//devtool: 'source-map',//开启sourceMap，方便调试
 	devServer: {
 		proxy: [
 			{
 				context: ['/api', '/aa'],//代理
-				target: process.env.TEST_SERVER_URL,//在dev.env中配置
+				target: process.env.CUSTOMIZE_TEST_SERVER_URL,//在dev.env中配置
 				changeOrigin: true,
 				pathRewrite: { '^/api': '/api' },//根据自己后台配置
 			},
 		],
+		port,
 	},
+	plugins: [
+		new webpack.DefinePlugin(devEnv?.parsed),
+	],
 });
-module.exports = defConfig;
+/*创建 webpack compiler*/
+const compiler = webpack(defConfig);
+// 创建 devServer
+const devServerOptions = Object.assign({}, defConfig?.devServer);
+const server = new WebpackDevServer(devServerOptions, compiler);
+// 启动 devServer
+server.start().then(() => {
+	console.log(`dev server listening on port ${port}`);
+	// 打开指定路由
+	const { exec } = require('child_process');
+	exec(`start http://localhost:${port}/#/home`); // 在 Windows 上打开 URL
+}).catch((err) => {
+	console.error(err);
+});
--- build/webpack.prod.js
+++ build/webpack.prod.js
@@ -4,8 +4,9 @@ const webpack = require('webpack');
 const baseConfig = require('./webpack.base.js');
 const TerserPlugin = require('terser-webpack-plugin');
 //使用dll，必须在打包前先生成manifest webpack --config .\build\webpack.dll.conf.js
-const manifest = require('./dist/vendor.manifest.json');
-const { FileListPlugin } = require('../plugin.js');
+//const manifest = require('./dist/vendor.manifest.json');
+const dotenv = require('dotenv');
+const prodEnv = dotenv.config({ path: 'dev.env', override: true });
 const prodConfig = merge(baseConfig, {
 	mode: 'production',
 	/* cdn分离依赖包 */
@@ -25,19 +26,19 @@ const prodConfig = merge(baseConfig, {
 					// 对使用的公共文件进行抽离
 					name: 'commons',
 					chunks: 'all',
-					minChunks: 2, // 最小公共次数
+					minChunks: 2, // 最小公共次数1
 				},
 			},
 		},
 	},
 	plugins: [
 		// 生成dll
-		new webpack.DllReferencePlugin({
+		/* new webpack.DllReferencePlugin({
 			context: __dirname,
 			manifest,// manifest 就是之前打包出来的 json 文件
-		}),
-		new FileListPlugin(),
+		}), */
 		/* css压缩 */
+		new webpack.DefinePlugin(prodEnv?.parsed),
 		new CssMinimizerPlugin({
 			test: /\.css$/,
 		}),
